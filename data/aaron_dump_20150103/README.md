da7317094d0308235e7cc8032fbdca3c62c3f6e0
Robert Chen <robchen401@gmail.com>
2015-01-03 20:58:51 -0500

    salty pickle

---
+++ data/aaron_dump_20150103/README.md
@@ -0,0 +1,4 @@
+## transformation of data gotten from BRFSS scraper
+
+* do this
+     python play_aarons_pickles.py   
--- data/aaron_dump_20150103/play_aarons_pickles.py
+++ data/aaron_dump_20150103/play_aarons_pickles.py
@@ -6,23 +6,25 @@
 
 #this is the pickle 
 pickle_filename = 'brfssData1996.pickle'
+output_filename = 'processed_brfssData1996.json'
 
 import os
 import sys
 import pickle
 from collections import OrderedDict
 import numpy as np
+import json
 
 l_raw_brfssData = pickle.load( open( pickle_filename, "rb" ) )
 
+###############
 d_question_data = OrderedDict()
-# make new data structure
 for idx in range(len(l_raw_brfssData)):
 	d_this_entry = l_raw_brfssData[idx]
 	s_this_questionEntry_string = d_this_entry['question']
 	s_this_questionEntry_key = d_this_entry['displayData'][0]['dataInfo']['qkey']
 	s_this_questionEntry_statename = d_this_entry['displayData'][0]['dataInfo']['state']
-	if s_this_question_key not in d_question_data:
+	if s_this_questionEntry_key not in d_question_data:
 		d_question_data[s_this_questionEntry_key] = dict()
 		d_question_data[s_this_questionEntry_key]['question'] = s_this_questionEntry_string
 		d_question_data[s_this_questionEntry_key]['answers_choices'] = d_this_entry['displayData'][0]['data'][0]['tableData'][0]
@@ -50,4 +52,8 @@ for idx in range(len(l_raw_brfssData)):
 				d_question_data[s_this_questionEntry_key]['answers_by_state'][s_this_questionEntry_statename][this_choice_name]['n'] = np.nan
 		else:
 			d_question_data[s_this_questionEntry_key]['answers_by_state'][s_this_questionEntry_statename][this_choice_name]['n'] = np.nan
+###############
 
+with open(output_filename, 'w') as outfile:
+	#json.dump(d_question_data, outfile)
+	json.dump(d_question_data, outfile,sort_keys=False, indent=4, separators=(',', ': '))
